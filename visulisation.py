71080deaa764fda0fb5882241a1104bdabb0673e
Raijohn <166853579+Raijohn@users.noreply.github.com>
2024-12-13 18:50:25 +0100

    visulisation.py

--- visulisation.py
+++ visulisation.py
@@ -23,12 +23,50 @@ plt.axis([0,10,0,20])
 plt.show()
 
 #mutiple lines on a single graph
-x = np.arange(1,11,0.01)
-plt.plot(x,x**2,"b-",label="plot²")
-plt.plot(x,x**3,"r-",label="plot³")
+x2 = np.arange(1,11,0.01)
+plt.plot(x2,x2**2,"b-",label="plot²")
+plt.plot(x2,x2**3,"r-",label="plot³")
 plt.legend()
 plt.show()
 
+#bar graph
+plt.bar([1,3,5,7],[21,94,62,84])
+plt.bar([2,4,6,8],[12,49,26,48])
+plt.show()
+#showing catagorical data in bar plot
+plt.bar(["male","female"],[10,7])
+plt.show()
+#histogram
+age = np.random.randint(1,80,20)
+print(age)
+bins = np.arange(1,81,10)
+plt.hist(age,bins,rwidth=0.8)
+plt.show()
+#scatter plot
+plt.scatter(x,y,color="blue",marker="x",s=40)
+plt.show()
+#pie chart
+piechart = np.random.randint(1,20,5)
+piechartsubject = ["pizza","burger","saled","spegetie","rice"]
+colors = ["c","m","g","r","b"]
+plt.pie(piechart,labels=piechartsubject,colors=colors,startangle=45,autopct='%1.1f%%',shadow=True)
+plt.show()
+#stack plot
+Days = ["Monday","thuesday","wednesday","thursday","friday"]
+eating = [2,4,1,3,5]
+studiing = [6,9,7,8,10]
+playing = [2,8,3,5,0]
+sleeping = [10,8,9,12,13]
+names = ["eating","studiing","playing","sleeping"]
+plt.stackplot(Days,eating,studiing,playing,sleeping,labels=names)
+plt.legend()
+plt.show()
+#subplots
+plt.subplot(2,2,3)
+plt.plot(x,y)
+plt.subplot(2,2,2)
+plt.pie(piechart,labels=piechartsubject,colors=colors,startangle=45,autopct='%1.1f%%',shadow=True)
+plt.show()
 #bar graph
 plt.bar([1,3,5,7],[21,94,62,84])
 plt.bar([2,4,6,8],[12,49,26,48])
